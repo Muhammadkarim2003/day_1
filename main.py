3a0c45ca36c131ce31e005302b3725b13bc34842
Sukhoi2003 <m2220134@gmail.com>
2024-04-19 23:35:50 +0500

    100 day python

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Guruh nomi generatori")
+
+shahar = input("Siz tug'ilgan shahar ")
+print(shahar)
+hayvon = input("Sizning uy hayvoningizning laqabi ")
+print(hayvon)
+
+print("Guruhingizni nomi ", shahar +" "+ hayvon)
